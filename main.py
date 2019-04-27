d1856ead83f65f03870158fe7ce00133915d41d9
mohit242 <f2014074@pilani.bits-pilani.ac.in>
2019-04-27 07:23:54 +0530

    fix: set train mode to false in play mode.

--- main.py
+++ main.py
@@ -87,7 +87,8 @@ if __name__ == "__main__":
         print("Loading network weights from file - {}".format(params["network_weight_file"]))
         agent.qnet_local.load_state_dict(torch.load(params["network_weight_file"]))
         score = 0
-        env_info = env.reset(train_mode=True)[brain_name]
+        env_info = env.reset(train_mode=False)[brain_name]
+
         state = env_info.vector_observations[0]
         while True:
             action = agent.act(state, eps=0)
@@ -95,7 +96,6 @@ if __name__ == "__main__":
             next_state = env_info.vector_observations[0]
             reward = env_info.rewards[0]
             done = env_info.local_done[0]
-            agent.step(state, action, reward, next_state, done)
             state = next_state
             score += reward
             if done:
